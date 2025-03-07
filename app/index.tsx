bf35337eda3033be99e1a4e179a0259e589b71a6
jswladitya <jswladitya0403@gmail.com>
2025-03-07 13:57:49 +0530

    Basics

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { styles } from "../styles/auth.styles";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello React native</Text>
+
+      {/* --> touchableOpacity gives you the popping up feedback card */}
+      {/* <TouchableOpacity onPress={() => alert("You Touched")}>
+        <Text>Press Me</Text>
+      </TouchableOpacity> */}
+
+
+      {/* --> Image Element : if image is in local system*/}
+      <Image source={require("../assets/images/icon.png")}
+      style={{width:100, height:100}}
+      />
+
+      {/* --> Image Element : if image is coming from the url*/}
+      {/* <Image source={{
+        uri: "Link-of-the-image"
       }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      style={{width:100, height:100}}
+      /> */}
+
+      
+      
+      {/* routing the other screen */}
+      <Link href={"/notifications"}>Visit notifications screen</Link>
     </View>
   );
 }
---
+++ app/profile.tsx
@@ -0,0 +1,10 @@
+// rnf : short cut for rn snippet
+import { View, Text } from 'react-native'
+
+export default function Profile() {
+  return (
+    <View>
+      <Text>profile screen</Text>
+    </View>
+  )
+}
